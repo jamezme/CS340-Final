724aeec0c2fc089c3a0869dc5eb62f80a96a3434
jamezme <jamesme@oregonstate.edu>
2024-12-03 12:36:16 -0800

    Displaying Members in index.php

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+	session_start();
+	//$currentpage="View Employees"; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Library DB</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+	<style type="text/css">
+        .wrapper{
+            width: 70%;
+            margin:0 auto;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+		 $('.selectpicker').selectpicker();
+    </script>
+</head>
+<body>
+    <?php
+        // Include config file
+        require_once "config.php";
+//		include "header.php";
+	?>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+		    <div class="page-header clearfix">
+		       <h2 class="pull-left">Member Details</h2>
+                        <a href="createEmployee.php" class="btn btn-success pull-right">Add New Member (TODO)</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "config.php";
+                    
+                    // Attempt select all employee query execution
+					// *****
+					// Insert your function for Salary Level
+					/*
+						$sql = "SELECT Ssn,Fname,Lname,Salary, Address, Bdate, PayLevel(Ssn) as Level, Super_ssn, Dno
+							FROM EMPLOYEE";
+					*/
+                    $sql = "SELECT MEMBER.Member_id, MEMBER.fname, MEMBER.lname, checked_out.Title
+							FROM MEMBER
+                            LEFT JOIN CHECK_OUT ON MEMBER.Member_id = CHECK_OUT.Member_id
+                            LEFT JOIN (SELECT BOOK.Title, BOOK.Book_id FROM BOOK NATURAL JOIN CHECK_OUT WHERE Return_date IS NULL)
+                            AS checked_out
+                            ON checked_out.Book_id = CHECK_OUT.Book_id";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo "<table class='table table-bordered table-striped'>";
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th width=8%>Member_id</th>";
+                                        echo "<th width=10%>First Name</th>";
+                                        echo "<th width=10%>Last Name</th>";
+                                        echo "<th width=10%>Checked Out</th>";
+                                        echo "<th width=10%>Action</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['Member_id'] . "</td>";
+                                        echo "<td>" . $row['fname'] . "</td>";
+                                        echo "<td>" . $row['lname'] . "</td>";
+                                        echo "<td>" . $row['Title'] . "</td>";
+                                        // echo "<td>";
+                                        //     echo "<a href='viewProjects.php?Ssn=". $row['Ssn']."&Lname=".$row['Lname']."' title='View Projects' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+                                        //     echo "<a href='updateEmployee.php?Ssn=". $row['Ssn'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                        //     echo "<a href='deleteEmployee.php?Ssn=". $row['Ssn'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+										// 	echo "<a href='viewDependents.php?Ssn=". $row['Ssn']."&Lname=".$row['Lname']."' title='View Dependents' data-toggle='tooltip'><span class='glyphicon glyphicon-user'></span></a>";
+                                        // echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo "<p class='lead'><em>No records were found.</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. <br>" . mysqli_error($link);
+                    }
+                    ?>
+                </div>
+
+</body>
+</html>
